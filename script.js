5bb66e50516d8e37eb062eef3173644f7a180b17
VikaKhaustova <haustovavika@gmail.com>
2024-09-19 15:01:45 +0300

    update2

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 
+// Отримуємо кнопку "Реєстрація" 
+let registerBtn = document.querySelector('.registration-icon');
+
+// Навішуємо обробник подій на клік кнопки "Реєстрація"
+
+    registerBtn.addEventListener("click", function () {
+        // Переходимо на сторінку реєстрації
+        window.location.assign('registration.html');
+    })
 
